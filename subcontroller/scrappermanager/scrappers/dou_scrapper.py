e0b1dbd32e08b01dc4f91d61d13da47b1b62a817
Felix-Grabovetskiy <felhsevov@gmail.com>
2021-04-18 12:58:37 +0300

    update dou

--- subcontroller/scrappermanager/scrappers/dou_scrapper.py
+++ subcontroller/scrappermanager/scrappers/dou_scrapper.py
@@ -2,6 +2,9 @@ from .scrapperInterface import *
 from datetime import datetime
 from dateutil import parser
 
+with open('city.json', 'r', encoding='utf-8') as f:
+    data = json.loads(f.read())
+
 
 options = webdriver.ChromeOptions()
 
@@ -39,7 +42,7 @@ class DouScrapper(ScrapperApi):
 
     def setFilters(self, filters: dict):
         self.filters = filters
-    
+
     def setToDefault(self):
         self.filters['vacancyInPack'] = 150
         self.filters['vacancyName'] = 'Developer'
@@ -50,83 +53,110 @@ class DouScrapper(ScrapperApi):
         self.cookies = self.driver.get_cookies()
 
     def getVacancysPack(self, pack_amount: int = 150, vacancy_name: Optional[str] = None):
-        self.driver.get('https://jobs.dou.ua/vacancies/')
-        self.driver.maximize_window()
-        self.driver.find_element_by_xpath(
-            '/div[2]/div[1]/div[2]/div[2]/form/span/input').send_keys(vacancy_name)
-        self.driver.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);")
-        self.driver.find_element_by_xpath(
-            '#container > div.header > div.b-sub-head-n > div.b-jobs-search > form > input'
-        ).click()
-        hrefs = self.driver.find_element_by_class_name('vt')
-        vacany_amount = len(hrefs)
-        while vacany_amount < pack_amount:
-            self.driver.find_element_by_css_selector(
-                '#vacancyListId > div > a').click()
-            hrefs = self.driver.find_element_by_class_name('vt')
-            vacany_amount = len(hrefs)
-        hrefs = [href.get_attribute('href') for href in hrefs]
-        data = []
-        data_params = {
-            'vacancy_name': [],
-            'salary': [],
-            'city': [],
-            'country': [],
-            'work_category': [],
-            'website_name': [],
-            'posted_date': [],
-            'effectivness': [],
-            'experience': [],
-        }
-        for href in hrefs:
-            self.driver.get(href)
+        try:
+          with open('dou.csv', 'w', newline='', encoding="UTF-8") as f:
+            fieldnames = ['vacancy_name', 'salary', 'city', 'country', 'work_category',
+                        'website_name', 'posted_date', 'effectivness']
+          thewriter = csv.DictWriter(f, fieldnames=fieldnames)
+          thewriter.writeheader()
+
+          driver.get('https://jobs.dou.ua')
+          time.sleep(2)
+
+          search = driver.find_element_by_class_name('job')
+          search.clear()
+          search.send_keys(info_search[0])
+          time.sleep(2)
+
+          search.send_keys(Keys.ENTER)
+          time.sleep(2)
+
+          items = driver.find_elements_by_class_name("vt")
+          hrefs = [href.get_attribute('href') for href in items]
+          print(hrefs)
+          data_params = {
+          'vacancy_name': [],
+          'salary': [],
+          'city': [],
+          'country': [],
+          'work_category': [],
+          'website_name': [],
+          'posted_date': [],
+          'effectivness': [],
+          }
+          for href in hrefs:
+            driver.get(href)
             time.sleep(2)
             data_params['effectivness'].append(0.0)
             data_params['website_name'].append('dou.ua')
-            data_params['experience'].append(1.0)
             data_params['work_category'].append('IT')
             data_params['country'].append('Ukraine')
 
             try:
-                name_vacansion = self.driver.find_element_by_class_name('g-h2')
-                data_params['vacancy_name'].append(name_vacansion.text)
-            except NoSuchElementException:
-                data_params['vacancy_name'].append('')
+              name_vacansion = driver.find_element_by_class_name('g-h2')
+              # print(f'Vacansion name:\n\t {name_vacansion.text}')
+              data_params['vacancy_name'].append(name_vacansion.text)
+            except Exception:
+              data_params['vacancy_name'].append('')
 
             try:
-                salary = self.driver.find_element_by_class_name('salary')
-                data_params['salary'].append(salary.text)
-            except NoSuchElementException:
-                data_params['salary'].append('')
+              salary = driver.find_element_by_class_name('salary')
+              # print(f'Salary:\n\t {salary.text}')
+              data_params['salary'].append(salary.text)
+            except Exception:
+              data_params['salary'].append('')
 
             try:
-                city = self.driver.find_element_by_class_name('place')
-                data_params['city'].append(city.text)
-            except NoSuchElementException:
-                data_params['city'].append('')
+              city = driver.find_element_by_class_name('place')
+              # print(f'City:\n\t {city.text}')
+              data_params['city'].append(city.text)
+            except Exception:
+              data_params['city'].append('')
 
             try:
-                posted_date = self.driver.find_element_by_class_name('date')
-                posted_date = posted_date.text.split(' ')
-                for month in data['months'].keys():
-                    if fuzz.ratio(month, posted_date[1]) >= 40:
-                        posted_date[1] = data['months'][month]
-                        if len(posted_date[0]) < 2:
-                            posted_date[0] = '0' + posted_date[0]
-                        year, month, day = list(
-                            map(int, '-'.join(reversed(posted_date)).split('-')))
-                        dt = datetime(year, month, day)
-                        timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
-                        data_params['posted_date'].append(timestamp)
-                        break
-                else:
-                    data_params['posted_date'].append(
-                        datetime.today().strftime('%Y-%m-%d'))
+              posted_date = driver.find_element_by_class_name('date')
+              posted_date = posted_date.text.split(' ')
+              # print(f'Date post:\n\t {posted_date}')
 
-            except NoSuchElementException as e:
-                print(e)
+              # data_params['posted_date'].append(posted_date.text)
+              for month in data['months'].keys():
+                if fuzz.ratio(month, posted_date[1]) >= 40:
+                  posted_date[1] = data['months'][month]
+                  if len(posted_date[0]) < 2:
+                    posted_date[0] = '0' + posted_date[0]
+                  year, month, day = list(
+                    map(int, '-'.join(reversed(posted_date)).split('-')))
+                  dt = datetime(year, month, day)
+                  timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+                  data_params['posted_date'].append(timestamp)
+                   # dt = datetime(','.join(reversed(posted_date)))
+                    # timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+                  break
+              else:
                 data_params['posted_date'].append(
-                    datetime.today().timestamp())
+                  datetime.today().strftime('%Y-%m-%d'))
 
-        return data_params
+            except Exception as e:
+              print(e)
+              data_params['posted_date'].append(
+                datetime.today().timestamp())
+
+          with open('dou.csv', 'a', newline='', encoding="UTF-8") as f:
+            fieldnames = ['vacancy_name', 'salary', 'city', 'country', 'work_category',
+                        'website_name', 'posted_date', 'effectivness']
+            thewriter = csv.DictWriter(f, fieldnames=fieldnames)
+            for vacancy in zip(*data_params.values()):
+              thewriter.writerow(
+              {'vacancy_name': vacancy[0], 'salary': vacancy[1], 'city': vacancy[2], 'country': vacancy[3],
+               'work_category': vacancy[4],
+               'website_name': vacancy[5], 'posted_date': vacancy[6], 'effectivness': vacancy[7]})
+            print('-------------------------------------------')
+            pprint(data_params)
+
+        except Exception as ex:
+          print(ex)
+
+        finally:
+          driver.close()
+          driver.quit()
+
